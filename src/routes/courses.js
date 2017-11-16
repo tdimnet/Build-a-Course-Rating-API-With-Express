1eada6aa49545297a57efe2cdb6a35b5743dcb09
tdimnet <thomas.dimnet@gmail.com>
2017-11-16 17:59:25 +0100

    Think I am ok with the route endpoints

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -10,11 +10,34 @@ router.get('/', function(req, res) {
   });
 });
 
+// POST Request to /api/courses
+router.post('/', function(req, res) {
+  res.json({
+    response: 'You sent me a POST request to "courses" endpoint.'
+  });
+});
+
 // GET Request to /api/courses/:courseId
 router.get('/:courseId', function(req, res) {
   res.json({
-    response: 'You sent me a GET request for a specific course ID: ' + req.params.courseId,
+    response: 'You sent me a GET request for a specific course ID: ' + req.params.courseId
   });
 });
+
+// PUT Request to /api/courses/:courseId
+router.put('/:courseId', function(req, res) {
+  res.json({
+    response: 'You sent me a PUT request for a specific course ID: ' + req.params.courseId
+  });
+});
+
+// POST Request to /api/courses/:courseId/reviews
+router.post('/:courseId/reviews', function(req, res) {
+  res.json({
+    response: 'You sent me a POST request for a specific course ID: ' + req.params.courseId + ' in order to add a review to it.'
+  });
+});
+
+
 
 module.exports = router;
