42de72a067739ed56303aa8a596286f4335f29ac
tdimnet <thomas.dimnet@gmail.com>
2017-11-16 17:49:48 +0100

    Sending get and post requests is now ok

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,10 +3,18 @@
 const express = require('express');
 const router = express.Router();
 
+// GET Request to /api/users
 router.get('/', function(req, res) {
-  res.json(({
+  res.json({
     response: 'You sent me a GET request to "users" endpoint.'
-  }))
+  });
+});
+
+router.post('/', function(req, res) {
+  res.json({
+    response: 'You sent me a POST request to "users" endpoint.',
+    body: req.body
+  });
 });
 
 // Exporting the router
