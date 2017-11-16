523a6136d2a9801d4f398782099e83692b34be23
tdimnet <thomas.dimnet@gmail.com>
2017-11-16 17:46:51 +0100

    Refacto code and target first get endpoint

--- src/index.js
+++ src/index.js
@@ -1,44 +1,45 @@
 'use strict';
 
-// load modules
-var express = require('express');
-var morgan = require('morgan');
-
-var app = express();
-
-// defining routes
-const users = require('./routes/users');
-
-// Body parser and logger
+// Import statements
+const express     = require('express');
+// Set up the express app
+const app         = express();
+// Import the route endpoints
+const users      = require('./routes/users');
+// Import body parser and logger
+const jsonParser  = require('body-parser').json;
 const logger      = require('morgan');
 
-// set our port
-app.set('port', process.env.PORT || 5000);
 
-// morgan gives us http request logging
-app.use(morgan('dev'));
+// Set up logger and body parser (for json encode)
+app.use(logger('dev'));
+app.use(jsonParser());
 
-// setup our static route to serve files from the "public" folder
-// app.use('/', express.static('public'));
+// Set up the router and the entry point: /questions
 app.use('/api/users', users);
 
-// catch 404 and forward to global error handler
-app.use(function(req, res, next) {
-  var err = new Error('File Not Found');
+// Catching up 404 erros
+app.use(function (req, res, next) {
+  const err = new Error('Not found!');
   err.status = 404;
   next(err);
 });
 
-// Express's global error handler
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+// Error Handler
+app.use(function (err, req, res, next) {
+  res.status(err.status  || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
   });
 });
 
-// start listening on our port
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server is listening on port ' + server.address().port);
+
+// Set up the port
+const port = process.env.PORT || 5000;
+
+// Running the app
+app.listen(port, function () {
+  console.log('Magic Happens on port', port);
 });
