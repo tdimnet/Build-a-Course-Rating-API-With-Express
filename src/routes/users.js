2d7113cae0b0c23e2bcbb598f4cccc36e80b1471
tdimnet <thomas.dimnet@gmail.com>
2017-12-20 20:06:00 +0100

    Return the correct user when GET /api/user/:userId

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,23 +7,28 @@ const User = require('../models/user');
 // GET /api/users
   // status: 200
   // goal: returns the currently authenticated user
-router.get('/', function(req, res, next) {
+router.get('/:userId', function(req, res, next) {
   User
-    .find({})
-    .exec(function(error, users) {
+    .findById(req.params.userId)
+    .exec(function(error, user) {
       if(error) {
-        return next(error);
+        res.status = 400;
+        res.json({
+          response: 'There is no user for this id'
+        });
       } else {
         res.status = 200;
         res.json({
-          response: users
+          response: user
         });
       }
     });
 
 });
 
-// POST Request to /api/users
+// POST /api/users
+  // status: 201
+  // goal: Creates a user, sets the Location header to "/", and returns no content
 router.post('/', function(req, res) {
   res.json({
     response: 'You sent me a POST request to "users" endpoint.',
