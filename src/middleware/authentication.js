6ab39bdc91269c059aa65e71a3dd8900dc463fee
tdimnet <thomas.dimnet@gmail.com>
2017-12-26 17:53:03 +0100

    Handle the bcrypt handler

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const auth = require('basic-auth');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+const requireAuth = function(req, res, next) {
+  // We create the exception handler
+  const unauthorized = new Error('Unauthorized User');
+  unauthorized.status = (401);
+
+
+  let credentials = auth(req);
+  if (credentials && credentials.name && credentials.pass) {
+
+    User
+      .findOne({ emailAddress: credentials.name })
+      .exec(function (err, user) {
+        if (err || !user) return next(unauthorized);
+
+        // if found, check for a password match
+        bcrypt.compare(credentials.pass, user.hashedPassword, function (err, check) {
+          // Attach the user id to the request and continue
+          if (check) {
+            req.userId = user._id;
+            return next();
+          }
+          return next(unauthorized);
+        });
+      });
+
+
+    } else {
+      // no credentials in the authorization header
+      return next(unauthorized);
+    }
+}
+
+module.exports.requireAuth = requireAuth;
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const requireAuth = require('../middleware/authentication');
 
 // GET /api/user
   // status: 200
