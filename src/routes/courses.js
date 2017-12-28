2f9724ebdd44418567911614fa311fcdcf72e4fb
tdimnet <thomas.dimnet@gmail.com>
2017-12-28 22:53:47 +0100

    Add the put method

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -104,12 +104,17 @@ router.put('/:courseId', mid.requireAuth, function(req, res, next) {
       course,
       function (error) {
         if (error && error.name === 'ValidationError') {
-          res.status(400);
-          res.json(validationErrors(400, error.errors));
+          res
+            .status(400)
+            .json({
+              response: error
+            })
         } else if (error) {
-          return next(err);
+          return next(error);
         } else {
-          res.status(204).send();
+          res
+            .status(204)
+            .send();
         }
       }
     );
@@ -128,32 +133,34 @@ router.put('/:courseId', mid.requireAuth, function(req, res, next) {
     // Sets the Location header to the related course
     // Returns no content
 router.post('/:courseId/reviews', mid.requireAuth, function(req, res, next) {
-  if (req.body.rating) {
-    const newReview = {
-      user: req.body.user || null,
-      rating: req.body.rating,
-      review: req.body.review || null
-    };
+  let review = new Review(req.body);
 
-    Review.create(newReview, function(error, review) {
-      if (error) {
-        return next(error);
-      } else {
+  review.save(function(error) {
+    if (error && error.name === 'ValidationError') {
         res
-          .status(201)
+          .status(400)
           .json({
-            response: newReview
+            response: error
           })
+    } else if (error) {
+      return next(error);
+    } else {
+      Course.findOne(
+        { _id: req.params.courseId },
+        function (error, course) {
+          if (error) return next(error);
+          course.reviews.push(review._id);
+          course.save(function (error, updated) {
+            if (error) return next(error);
+            res
+              .status(201)
+              .location('/courses/' + updated._id)
+              .send();
+          });
+        });
       }
-    });
-
-  } else {
-    res
-      .status(403)
-      .json({
-        error: 'All fields are required'
-      })
-  }
+    }
+  );
 });
 
 
